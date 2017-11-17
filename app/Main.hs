fc4733fade1351a81c325403ebf743a6e9baf86a
Sebastian Ertel <sebastian.ertel@gmail.com>
2017-11-17 09:59:11 +0100

    compiler flags that output core.

--- app/Main.hs
+++ app/Main.hs
@@ -12,15 +12,20 @@ import Control.Monad.State
 --   r <- peek resultPtr
 --   print $ "result:" ++ show r
 
+-- main :: IO ()
+-- main = let xs = [0..9]
+--            zero = 0 :: Int
+--            s = S zero zero zero
+--        in do
+--         print "With ffi:"
+--         print $ runState (smap (f3'.@.f2'.@.f1') xs) s
+--         print "Without ffi:"
+--         print $ runState (smap (f3.@.f2.@.f1) xs) s
+
 main :: IO ()
 main = let xs = [0..9]
            zero = 0 :: Int
            s = S zero zero zero
        in do
         print "With ffi:"
-        print $ runState (smap (f3'.@.f2'.@.f1') xs) s
-        print "Without ffi:"
-        print $ runState (smap (f3.@.f2.@.f1) xs) s
-        -- print $ runState (smap (f1.@.f3.@.f2) xs) s
-        -- print $ runState (smap (f1.+.g1) xs) s
-        -- print $ runState (smap (g1.+.f1) xs) s
+        print $ runState (smap (f2'.@.f1') xs) s
