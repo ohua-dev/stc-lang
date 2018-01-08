0b5efdfbe7fc2dad149712147e069cf9d7fabd4a
Justus Adam <dev@justus.science>
2018-01-08 18:10:20 +0100

    Implemented smap primitive in AST and runtime

--- src/StreamsBasedFreeMonad.hs
+++ src/StreamsBasedFreeMonad.hs
@@ -11,6 +11,7 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE InstanceSigs               #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE TupleSections              #-}
 {-# LANGUAGE TypeFamilies               #-}
 {-# LANGUAGE TypeOperators              #-}
 {-# LANGUAGE TypeSynonymInstances       #-}
@@ -31,13 +32,13 @@ import           Control.Monad.Writer     as W
 --
 import           Control.Concurrent.Async
 import           Control.Concurrent.Chan
-import           Control.Concurrent.MVar
 import           Control.Exception
 import           Control.Monad.Free
 import           Control.Parallel         (pseq)
 import           Data.Dynamic
 import           Data.Either
 import           Data.Function
+import           Data.IORef
 import           Data.Kind
 import           Data.List
 import qualified Data.Map                 as Map
@@ -188,6 +189,13 @@ data ASTAction globalState a
         (Accessor globalState sfState)
         [Var Void] -- reference to the input vars
         (Var returnType -> a) -- continutation
+    | forall inputType returnType .
+        (Typeable inputType, Typeable returnType)
+        =>
+        Smap
+            (Var inputType -> ASTM globalState (Var returnType))
+            (Var [inputType])
+            (Var [returnType] -> a)
 
 data TaggedInt a = TaggedInt Integer deriving (Show, Eq, Ord)
 
@@ -196,6 +204,7 @@ instance Num (TaggedInt a) where
 
 instance Functor (ASTAction globalState) where
     fmap f (InvokeSf sf tag vars ac) = InvokeSf sf tag vars (f . ac)
+    fmap f (Smap f1 v cont)          = Smap f1 v (f . cont)
 newtype Var t = Var { unwrapVar :: Unique }
 -- make state be input
 data Sf state (inputTypes :: [Type]) returnType
@@ -206,7 +215,17 @@ data Sf state (inputTypes :: [Type]) returnType
     , Typeable returnType
     , ApplyVars fnType
     ) => Sf fnType -- reference to the actual function
-data Algorithm globalState a = Algorithm [(SFRef globalState, [Unique], Unique)] Unique
+data NodeType globalState
+    = CallSf (SFRef globalState)
+    | forall inputType . Typeable inputType => SmapBegin (Proxy inputType)
+    | forall outputType . Typeable outputType => Collect (Proxy outputType)
+    | forall inputType . Typeable inputType => Size (Proxy inputType)
+data Node globalState = Node
+    { nodeType  :: NodeType globalState
+    , inputRefs :: [Unique]
+    , outputRef :: Unique
+    }
+data Algorithm globalState a = Algorithm [Node globalState] Unique
 newtype Unique = Unique { uniqueToInt :: Int } deriving (Show, Eq, Ord)
 
 instance Enum Unique where
@@ -291,6 +310,9 @@ call sf accessor = collectVars [] $ \vars -> liftF $ InvokeSf sf accessor (rever
 toVoidVar :: Var a -> Var Void
 toVoidVar (Var u) = Var u
 
+smap :: (Typeable a, Typeable b) => (Var a -> ASTM globalState (Var b)) -> Var [a] -> ASTM globalState (Var [b])
+smap f lref = liftF $ Smap f lref id
+
 -- | Helper class to make 'call' a multi arity function
 class CollectVars t where
     type Ret t
@@ -317,22 +339,40 @@ stag = Accessor (const ()) const
 
 sf1 :: ASTM s (Var T)
 sf1 = call (liftSf (liftIO (putStrLn "Executing sf1") >> pure T :: SFMonad () T)) stag
-sf2 :: Var T -> ASTM s (Var A)
-sf2 = call (liftSf (\T -> liftIO (putStrLn "Executing sf2") >> return A :: SFMonad () A)) stag
-sf3 :: Var T -> Var A -> ASTM s (Var B)
-sf3 = call (liftSf (\T A -> liftIO (putStrLn "Executing sf3") >> return B :: SFMonad () B)) stag
+sf2 :: Var T -> ASTM s (Var [Int])
+sf2 = call (liftSf (\T -> liftIO (putStrLn "Executing sf2") >> return [0..20] :: SFMonad () [Int])) stag
+sf3 :: Var T -> Var Int -> ASTM s (Var Int)
+sf3 = call (liftSf (\T i -> liftIO (putStrLn "Executing sf3") >> return (succ i) :: SFMonad () Int)) stag
 createAlgo :: ASTM s (Var a) -> Algorithm s a
 createAlgo (ASTM m) = Algorithm w retVar
   where
-    (Var retVar, _, w) = runRWS (iterM f m) () (Unique 0)
-    f (InvokeSf sf tag vars cont) = do
-        retRef <- freshUnique
-        tell $ pure
-            ( SFRef sf tag
-            , map unwrapVar vars
-            , retRef
-            )
-        cont $ Var retRef
+    (Var retVar, _, w) = runRWS (iterM go m) () (Unique 0)
+    go :: ASTAction s (RWS () [Node s] Unique b) -> RWS () [Node s] Unique b
+    go (InvokeSf sf tag vars cont) =
+        continue cont $ Node (CallSf $ SFRef sf tag) (map unwrapVar vars)
+    go (Smap (innerCont :: Var inputType -> ASTM globalState (Var returnType)) (Var inputVar) cont) = do
+        innerContVar <- freshUnique
+        retVar <- freshUnique
+        sizeRet <- freshUnique
+        let iproxy = Proxy :: Proxy inputType
+        tell
+            [ Node (SmapBegin iproxy) [inputVar] retVar
+            , Node (Size iproxy) [inputVar] sizeRet
+            ]
+        let ASTM inner = innerCont (Var innerContVar)
+        v <- iterM go inner
+        collReturn <- freshUnique
+        -- tell $ pure $ Node Collect [unwrapVar v] collReturn
+        -- cont (Var collReturn)
+        continue cont (Node (Collect (Proxy :: Proxy returnType)) [sizeRet, unwrapVar v])
+
+    continue :: (Var t -> RWS () [Node s] Unique b)
+             -> (Unique -> Node s)
+             -> RWS () [Node s] Unique b
+    continue cont f = do
+        u <- freshUnique
+        tell $ pure $ f u
+        cont (Var u)
 
 
 class ApplyVars t where
@@ -340,8 +380,8 @@ class ApplyVars t where
 
 
 instance (ApplyVars b, Typeable a) => ApplyVars (a -> b) where
-    applyVars func (x:xs) = applyVars (maybe (error "Coercion failed") func $ fromDynamic x) xs
-    applyVars _ [] = error "too few arguments"
+    applyVars func (x:xs) = applyVars (func $ forceDynamic x) xs
+    applyVars _ []        = error "too few arguments"
 
 
 instance ApplyVars (SFMonad state retType) where
@@ -349,11 +389,20 @@ instance ApplyVars (SFMonad state retType) where
     applyVars _ _    = error "Too many arguments"
 
 
+
+forceDynamic :: forall a . Typeable a => Dynamic -> a
+forceDynamic dyn =
+    case fromDynamic dyn of
+        Just a -> a
+        Nothing -> error $ "Mismatching types. Expected " ++ show rep ++ " got " ++ show (dynTypeRep dyn)
+  where rep = typeRep (Proxy :: Proxy a)
+
+
 runAlgo :: Typeable a => Algorithm globalState a -> globalState -> IO a
 runAlgo (Algorithm nodes retVar) st = do
-    stateVar <- newMVar st
-    let outputMap = Map.fromList $ zip (map (\(_, _, o) -> o) nodes) (repeat []) :: Map.Map Unique [Chan (Either () Dynamic)]
-        f (m, l) (func, inputs, oUnique) = do
+    stateVar <- newIORef st
+    let outputMap = Map.fromList $ zip (map outputRef nodes) (repeat []) :: Map.Map Unique [Chan (Either () Dynamic)]
+        f (m, l) (Node func inputs oUnique) = do
             inChans <- mapM (const newChan) inputs
             let newMap = foldl (\m (u, chan) -> Map.update (Just . (chan:)) u m) m (zip inputs inChans)
             pure (newMap, (func, inChans, oUnique):l)
@@ -364,48 +413,101 @@ runAlgo (Algorithm nodes retVar) st = do
     let finalMap = Map.update (Just . (retChan:)) retVar outMap
 
     bracket
-        (mapM (async . \(SFRef sf tag, inChans, retUnique :: Unique) -> runFunc sf tag inChans (fromMaybe (error "return value not found") $ Map.lookup retUnique finalMap) stateVar) funcs)
+        (mapM (async . \(nt, inChans, retUnique :: Unique) ->
+            let outChans = fromMaybe (error "return value not found") $ Map.lookup retUnique finalMap
+                sendResult val = mapM_ (`writeChan` val) outChans
+                sendFinish = sendResult (Left ())
+                withValueFrom chan cont = readChan chan >>= either (const sendFinish) cont
+
+                -- FIXME only send finish after list is done
+                withNValuesFrom n0 chan cont = loop n0 []
+                  where
+                    loop 0 l = cont (reverse l)
+                    loop n l
+                        | n < 0 = error "invariant broken"
+                        | otherwise = withValueFrom chan (loop (pred n) . (:l))
+            in
+            case nt of
+                CallSf (SFRef sf accessor) -> runFunc sf accessor stateVar pullInputs sendResult
+                  where
+                    func = runFunc sf accessor stateVar
+                    pullInputs
+                        | null inChans = pure []
+                        | otherwise = mapM readChan inChans
+                SmapBegin (Proxy :: Proxy inputType)
+                    | [x] <- inChans ->
+                        let loop = withValueFrom x $ \y -> do
+                                let ylist = forceDynamic y :: [inputType]
+                                    dynYList = map toDyn ylist
+                                mapM_ (sendResult . Right) dynYList
+                                loop
+                        in loop
+                    | otherwise -> error $ "Wrong number of input arguments to smap (" ++ show (length inChans) ++ ")"
+                Collect (Proxy :: Proxy returnType)
+                    | [sizeChan, x] <- inChans ->
+                        let loop = withValueFrom sizeChan $ \size ->
+                                   withNValuesFrom (forceDynamic size :: Int) x $ \y -> do
+                                        let ylist = map forceDynamic y :: [returnType]
+                                        sendResult $ Right $ toDyn ylist
+                                        loop
+                        in loop
+                    | otherwise -> error $ "Wrong number of input arguments to collect (" ++ show (length inChans) ++ ")"
+                Size (Proxy :: Proxy inputType)
+                    | [x] <- inChans ->
+                        let loop = withValueFrom x $ sendResult . Right . dynApp (toDyn (length :: [inputType] -> Int)) >=> const loop
+                        in loop
+
+                    | otherwise -> error $ "Wrong number of input arguments to size (" ++ show (length inChans) ++ ")"
+            ) funcs)
         (mapM_ cancel)
         $ \threads -> do
             mapM_ link threads
             Right ret <- readChan retChan
             Left () <- readChan retChan
             mapM_ cancel threads
-            pure $ fromMaybe (error "wrong type") $ fromDynamic ret
+            pure $ forceDynamic ret
 
 
-runFunc :: forall state inputTypes retType globalState.
-        Sf state inputTypes retType
+
+
+atomicModifyIORef'_ :: IORef a -> (a -> a) -> IO ()
+atomicModifyIORef'_ ref f = atomicModifyIORef' ref ((, ()) . f)
+
+
+runFunc :: forall state inputTypes retType globalState
+        .  Sf state inputTypes retType
         -> (Accessor globalState state)
-        -> [Chan (Either () Dynamic)] -> [Chan (Either () Dynamic)]
-        -> MVar globalState -> IO ()
-runFunc (Sf (f :: f)) accessor inChans outChans stTrack = loop
+        -> IORef globalState
+        -> IO [Either () Dynamic]
+        -> (Either () Dynamic -> IO ())
+        -> IO ()
+runFunc (Sf (f :: f)) accessor stTrack pullInputs sendResult = loop
   where
     runFuncOnce inVars = do
-        s <- takeMVar stTrack
+        s <- readIORef stTrack
         (ret, newState) <- runStateT (runSFMonad (applyVars f inVars)) (getter accessor s)
-        putMVar stTrack (setter accessor s newState)
+        atomicModifyIORef'_ stTrack (\s -> setter accessor s newState)
         pure ret
-    sendResult ret = mapM_ (`writeChan` Right (toDyn (ret :: retType))) outChans
-    sendFinish = mapM_ (`writeChan` Left ()) outChans
-    loop | null inChans = do
-            runFuncOnce [] >>= sendResult
-            sendFinish
-         | otherwise = do
-            allIn <- mapM readChan inChans
+    sendFinish = sendResult (Left ())
+    sendValue = sendResult . Right . toDyn
+    loop = do
+            allIn <- pullInputs
             case partitionEithers allIn of
+                ([], []) -> do
+                    runFuncOnce [] >>= sendValue
+                    sendFinish
                 ([], inVars) -> do
-                    runFuncOnce inVars >>= sendResult
+                    runFuncOnce inVars >>= sendValue
                     loop
                 (_, []) -> sendFinish
                 _ -> error "Some but not all inputs were closed"
 
 
-algorithm :: Algorithm () B
+algorithm :: Algorithm () [Int]
 algorithm = createAlgo $ do
     var1 <- sf1
     var3 <- sf2 var1
-    v <- sf3 var1 var3
+    v <- smap (sf3 var1) var3
     pure v
 
 freshUnique :: MonadState Unique m => m Unique
@@ -418,6 +520,12 @@ data SFRef globalState
     SFRef (Sf state inputTypes retType) (Accessor globalState state)
 
 printGraph (Algorithm gr ret) = do
-    forM_ gr $ \(SFRef _ _, vars, sfRet) ->
-        putStrLn $ "Sf with inputs " ++ show (map uniqueToInt vars) ++ " returns " ++ show (uniqueToInt sfRet)
+    forM_ gr $ \(Node nt vars sfRet) ->
+        let ntStr = case nt of
+                        CallSf _    -> "Sf"
+                        SmapBegin _ -> "Smap"
+                        Collect _   -> "Collect"
+                        Size _      -> "Size"
+        in
+        putStrLn $ ntStr ++ " with inputs " ++ show (map uniqueToInt vars) ++ " returns " ++ show (uniqueToInt sfRet)
     putStrLn $ "last return is " ++ show (uniqueToInt ret)
