8668a1515685b53adf6cbe8035d9e0bcc40da89b
Justus Adam <dev@justus.science>
2018-03-23 20:58:56 +0100

    Formatted Magic modules
    Also added the implementation for `injectList`

--- src/Type/Magic/GHC8.hs
+++ src/Type/Magic/GHC8.hs
@@ -1,40 +1,58 @@
-{-# LANGUAGE ExplicitForAll      #-}
-{-# LANGUAGE GADTs               #-}
-{-# LANGUAGE KindSignatures      #-}
-{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE TypeApplications    #-}
-module Type.Magic.GHC8 (injectList, extractFunctor, extractList, injectFunctor) where
+{-# LANGUAGE TypeApplications #-}
 
-import           Control.Exception
-import           Data.Dynamic2     (Dynamic (..), TypeCastException (..))
-import           Data.Kind
-import           Type.Reflection
-import           Unsafe.Coerce
+module Type.Magic.GHC8
+    ( injectList
+    , extractFunctor
+    , extractList
+    , injectFunctor
+    ) where
 
+import Control.Exception
+import Data.Dynamic2 (Dynamic(..), TypeCastException(..))
+import Data.Kind
+import Type.Reflection
+import Unsafe.Coerce
 
-extractFunctor :: forall f . (Typeable f, Functor f) => Dynamic -> f Dynamic
-extractFunctor = \case
-  Dynamic (SomeTypeRep (App con tra)) dl ->
-    case con `eqTypeRep` targetRep of
-      Just HRefl -> fmap f $ unsafeCoerce dl
-      Nothing -> throw $ TypeCastException (SomeTypeRep con) (SomeTypeRep targetRep)
-    where
-      f = Dynamic (SomeTypeRep tra)
-  Dynamic tr _ -> throw $ TypeCastException tr (SomeTypeRep targetRep)
+extractFunctor ::
+       forall f. (Typeable f, Functor f)
+    => Dynamic
+    -> f Dynamic
+extractFunctor =
+    \case
+        Dynamic (SomeTypeRep (App con tra)) dl ->
+            case con `eqTypeRep` targetRep of
+                Just HRefl -> fmap f $ unsafeCoerce dl
+                Nothing ->
+                    throw $
+                    TypeCastException (SomeTypeRep con) (SomeTypeRep targetRep)
+            where f = Dynamic (SomeTypeRep tra)
+        Dynamic tr _ -> throw $ TypeCastException tr (SomeTypeRep targetRep)
   where
     targetRep = typeRep @f
 
 extractList :: Dynamic -> [Dynamic]
 extractList = extractFunctor
 
-injectFunctor :: forall f . (Typeable f, Functor f) => Dynamic -> f Dynamic -> Dynamic
-injectFunctor (Dynamic (SomeTypeRep tra) _) l = Dynamic tr $ unsafeCoerce $ fmap unwrap l
+injectFunctor ::
+       forall f. (Typeable f, Functor f)
+    => Dynamic
+    -> f Dynamic
+    -> Dynamic
+injectFunctor (Dynamic (SomeTypeRep tra) _) l =
+    Dynamic tr $ unsafeCoerce $ fmap unwrap l
   where
     unwrap (Dynamic _ v) = v
-    tr = case traKind `eqTypeRep` kindStar of
-           Just HRefl -> SomeTypeRep $ App (typeRep @f) tra
-           Nothing    -> throw $ TypeCastException (SomeTypeRep traKind) (SomeTypeRep kindStar)
+    tr =
+        case traKind `eqTypeRep` kindStar of
+            Just HRefl -> SomeTypeRep $ App (typeRep @f) tra
+            Nothing ->
+                throw $
+                TypeCastException (SomeTypeRep traKind) (SomeTypeRep kindStar)
       where
         traKind = typeRepKind tra
         kindStar = typeRep @Type
--- src/Type/Magic/OldGHC.hs
+++ src/Type/Magic/OldGHC.hs
@@ -1,50 +1,57 @@
-{-# LANGUAGE BangPatterns        #-}
-{-# LANGUAGE ExplicitForAll      #-}
-{-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE BangPatterns #-}
+{-# LANGUAGE ExplicitForAll #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE ScopedTypeVariables #-}
+
 module Type.Magic.OldGHC
-  (extractList, injectList, extractFunctor, injectFunctor
-  ) where
+    ( extractList
+    , injectList
+    , extractFunctor
+    , injectFunctor
+    ) where
 
+import Control.Exception
+import Data.Dynamic2 (Dynamic(..), TypeCastException(..))
+import Data.Maybe
+import Data.Tuple.OneTuple
+import Data.Typeable
+import Debug.Trace
+import GHC.Exts (Any)
+import Unsafe.Coerce
 
-import           Control.Exception
-import           Data.Dynamic2       (Dynamic (..), TypeCastException (..))
-import           Data.Maybe
-import           Data.Tuple.OneTuple
-import           Data.Typeable
-import           Debug.Trace
-import           GHC.Exts            (Any)
-import           Unsafe.Coerce
-
-
-extractFunctor :: forall f . (Typeable f, Functor f) => Dynamic -> f Dynamic
-extractFunctor = \(Dynamic trl dl) ->
-  let
-    (tyCon, tyArgs) = splitTyConApp trl
-  in
-    if tyCon == expectedTyCon
-    then
-      case tyArgs of
-        [tra] -> fmap f l
-          where
-            f = Dynamic tra
-            l = unsafeCoerce dl
-        _ -> error $ "Wrong kind for constructor " ++ show tyCon
-    else throw $ TypeCastException expectedTy (mkTyConApp tyCon [])
+extractFunctor ::
+       forall f. (Typeable f, Functor f)
+    => Dynamic
+    -> f Dynamic
+extractFunctor =
+    \(Dynamic trl dl) ->
+        let (tyCon, tyArgs) = splitTyConApp trl
+         in if tyCon == expectedTyCon
+                then case tyArgs of
+                         [tra] -> fmap f l
+                             where f = Dynamic tra
+                                   l = unsafeCoerce dl
+                         _ ->
+                             error $ "Wrong kind for constructor " ++ show tyCon
+                else throw $ TypeCastException expectedTy (mkTyConApp tyCon [])
   where
     !expectedTy = typeOf (Proxy :: Proxy f)
     !expectedTyCon = typeRepTyCon expectedTy
 
-
 extractList :: Dynamic -> [Dynamic]
 extractList = extractFunctor
 
-injectFunctor :: forall f . (Typeable f, Functor f) => Dynamic -> f Dynamic -> Dynamic
+injectFunctor ::
+       forall f. (Typeable f, Functor f)
+    => Dynamic
+    -> f Dynamic
+    -> Dynamic
 injectFunctor (Dynamic tra _) l = Dynamic tr $ unsafeCoerce $ fmap unwrap l
   where
     unwrap (Dynamic _ v) = v
     tr = mkTyConApp (typeRepTyCon (typeOf (Proxy :: Proxy f))) [tra]
 
-
+injectList :: [Dynamic] -> Dynamic
 injectList [] = error "Cannot convert empty list yet"
+injectList l@(x:_) = injectFunctor x l
 
