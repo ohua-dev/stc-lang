316a06f2f62bfb1620937210aad09dbc807b7a48
Sebastian Ertel <sebastian.ertel@gmail.com>
2019-04-30 16:52:31 +0200

    adding the other benchmarks for par

--- benchmarks/src/fbm_parallelism_runner.hs
+++ benchmarks/src/fbm_parallelism_runner.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 
 import Control.Monad
 import Criterion
@@ -6,6 +7,7 @@ import Criterion.IO
 import Criterion.Types
 import Data.Aeson
 import qualified Data.ByteString.Lazy as B
+import qualified Data.HashMap as HM
 import Data.List
 import qualified Data.Vector as V
 import System.Environment
@@ -13,10 +15,10 @@ import System.Exit
 import System.IO
 import System.Process as P
 
-toScriptFormat :: [(Word, [Report])] -> Value
-toScriptFormat = toJSON . concatMap mapRun
+toScriptFormat :: [String] -> [(Word, [Report])] -> Value
+toScriptFormat variants = toJSON . concatMap (mapRun variants)
   where
-    mapRun (cores, reps) =
+    mapRun variants (cores, reps) =
       map
         (\(exp, rep) ->
            object
@@ -30,11 +32,12 @@ toScriptFormat = toJSON . concatMap mapRun
                          ["start" .= (0.0 :: Float), "finish" .= measTime m]) $
                   reportMeasured rep)
              ]) $
-      zip ["sequential", "pure", "reading", "writing"] reps
+      zip variants reps
 
 reportFileName = "results/reports.json"
 
-runExperiment lo hi fname benchmark = do
+runExperiment lo hi benchmark variants = do
+  let fname = benchmark ++ "-" ++ show lo ++ ":" ++ show hi ++ "-results"
   recs <-
     forM [lo .. hi] $ \c -> do
       callProcess
@@ -53,23 +56,43 @@ runExperiment lo hi fname benchmark = do
         let ms = reportMeasured r
          in sum (measTime <$> ms) / fromIntegral (V.length ms)
       outliers = ovEffect . anOutlierVar . reportAnalysis
-  writeFile (fname ++ ".csv") $
+  writeFile ("results/" ++ fname ++ ".csv") $
     unlines $
     map
       (\(c, recs) ->
          intercalate "," $
          show c : map (show . avg) recs ++ map (show . outliers) recs)
       recs
-  B.writeFile (fname ++ ".json") $ encode $ toScriptFormat recs
+  B.writeFile ("results/" ++ fname ++ ".json") $
+    encode $ toScriptFormat variants recs
 
 -- forM ["ohua-bench", "comp-bench", "app-bench", "cond-bench"] $ \benchmark -> do
-main = do
-  [name, cs] <- getArgs
-  let (lo, hi) = read cs
-  let fname = name ++ "-" ++ show lo ++ ":" ++ show hi ++ "-results"
+setUpExperiment = do
   callProcess "stack" ["build"]
   callProcess
     "stack"
     ["ghc", "--", "src/fbm_data_parallelism.hs", "-rtsopts", "-threaded"]
-  runExperiment lo hi fname name
+
+experiments =
+  [ ("ohua-bench", ["sequential", "pure", "reading", "writing"])
+  , ("comp-bench", ["sequential", "ohua", "par1", "par2"])
+  , ("app-bench", ["sequential", "ohua", "par"])
+  , ("cond-bench", ["sequential", "ohua", "par"])
+  ]
+
+runAllExperiments lo hi = do
+  forM experiments $ \(name, variants) -> runExperiment lo hi name variants
+
+main = do
+  [name, cs] <- getArgs
+  let (lo, hi) = read cs
+  setUpExperiment
+  if name == "all"
+    then runAllExperiments lo hi
+    else (\x -> return [x]) =<< (runExperiment lo hi name $ getVariants name)
   where
+    getVariants benchmark =
+      let variants = HM.lookup benchmark (HM.fromList experiments)
+       in case variants of
+            (Just v) -> v
+            (Nothing) -> error $ "No such benchmark: " ++ benchmark
