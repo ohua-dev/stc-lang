d9cf6fd3561079368dd0150e03249b16d8bfa14d
Sebastian Ertel <sebastian.ertel@gmail.com>
2019-05-03 17:11:03 +0200

    a mapReduce implementation

--- src/Monad/FuturesBasedMonad.hs
+++ src/Monad/FuturesBasedMonad.hs
@@ -500,3 +500,55 @@ filterSignal init cond f = do
     r <- cond item
     i <- case_ r [(True, Just <$> f item), (False, pure Nothing)]
     g i
+
+----
+-- The below comes originally from: https://hackage.haskell.org/package/monad-par-extras-0.3.3/docs/src/Control-Monad-Par-Combinator.html#parMapReduceRangeThresh
+----
+data InclusiveRange =
+  InclusiveRange Int
+                 Int
+
+-- | Computes a binary map\/reduce over a finite range.  The range is
+-- recursively split into two, the result for each half is computed in
+-- parallel, and then the two results are combined.  When the range
+-- reaches the threshold size, the remaining elements of the range are
+-- computed sequentially.
+--
+-- For example, the following is a parallel implementation of
+--
+-- >  foldl (+) 0 (map (^2) [1..10^6])
+--
+-- > parMapReduceRangeThresh 100 (InclusiveRange 1 (10^6))
+-- >        (\x -> return (x^2))
+-- >        (\x y -> return (x+y))
+-- >        0
+--
+parMapReduceRangeThresh ::
+     (NFData a, ParFuture iv p)
+  => Int -- ^ threshold
+  -> InclusiveRange -- ^ range over which to calculate
+  -> (Int -> p a) -- ^ compute one result
+  -> (a -> a -> p a) -- ^ combine two results (associative)
+  -> a -- ^ initial result
+  -- -> p a
+  -> p a
+parMapReduceRangeThresh threshold (InclusiveRange min max) fn binop init = do
+  let (, [reduceState]]) = runOhuaM mapReduce
+  in reduceState
+  where
+    mapReduce = smapGen_ (mapAndCombine >=> reduce) chunkGenerator
+  -- loop min max
+  -- where
+  --   loop min max
+  --     | max - min <= threshold =
+  --       let mapred a b = do
+  --             x <- fn b
+  --             result <- a `binop` x
+  --             return result
+  --        in foldM mapred init [min .. max]
+  --     | otherwise = do
+  --       let mid = min + ((max - min) `quot` 2)
+  --       rght <- spawn $ loop (mid + 1) max
+  --       l <- loop min mid
+  --       r <- get rght
+  --       l `binop` r
